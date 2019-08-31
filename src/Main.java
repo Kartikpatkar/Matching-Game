1126e8250ca82503f086fee0bfa7779a991b2b6e
Kartik Patkar <45987061+Kartikpatkar@users.noreply.github.com>
2019-08-31 17:26:40 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import views.matchingGame;
+
+public class Main
+{
+    public static void main(String[] argv)
+    {
+        matchingGame instance = new matchingGame();
+        instance.newGame();
+    }
+}
